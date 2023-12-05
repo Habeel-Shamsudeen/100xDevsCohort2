2068f423274b53c8cdc0ad25a3195cf0a2536c9e
Habeel-Shamsudeen <habeelshamsudeen9895@gmail.com>
2023-12-05 19:07:15 +0530

    Easy Expenditure completed

--- Assignments/01-js/easy/expenditure-analysis.js
+++ Assignments/01-js/easy/expenditure-analysis.js
@@ -6,7 +6,35 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  let result=[];
+  
+  for(let i=0;i<transactions.length;i++){
+    let transCat=transactions[i].category;
+    let transPrice=transactions[i].price;
+    //let index=checkCat(result,transCat);
+    let index=result.findIndex(obj=> obj.category===transCat); // I think here obj acts like each element of result it is the argument of the arrow function
+    // function is run for each element in the array with obj(array element as argument)
+    console.log(index);
+    if(index>-1){
+      result[index].totalSpent+=transPrice;
+    }else{
+      let object={
+        category:transCat,
+        totalSpent:transPrice
+      };
+      result.push(object);
+    }
+  }
+  return result;
+}
+
+function checkCat(arr,cat){
+  for(let i=0;i<arr.length;i++){
+    if(arr[i].category===cat){
+      return i;
+    }
+  }
+  return -1;
 }
 
 module.exports = calculateTotalSpentByCategory;
