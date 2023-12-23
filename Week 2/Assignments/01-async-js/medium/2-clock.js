970fd2b33b62ff61aa260995274b9b380382eb09
Habeel-Shamsudeen <habeelshamsudeen9895@gmail.com>
2023-12-23 13:09:08 +0530

    medium week2 completed

--- Week 2/Assignments/01-async-js/medium/2-clock.js
+++ Week 2/Assignments/01-async-js/medium/2-clock.js
@@ -6,3 +6,16 @@
 //  - HH:MM::SS (Eg. 13:45:23)
 
 //  - HH:MM::SS AM/PM (Eg 01:45:23 PM)
+
+function clock(){
+    let date = new Date();
+    time = date.getHours()+":"+date.getMinutes()+":"+date.getSeconds()+" ";
+    formatedTime=(date.getHours()%12)+":"+date.getMinutes()+":"+date.getSeconds()+" ";
+    console.log(time);
+    M=(date.getHours >=12) ? "AM" : "PM";
+    console.log(formatedTime+" "+ M);
+}
+setInterval(()=>{
+    clock();
+},1*1000);
+
