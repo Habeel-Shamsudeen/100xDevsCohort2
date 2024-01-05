90d5b420e2d694361d8be406bde251a02c727bf2
Habeel-Shamsudeen <habeelshamsudeen9895@gmail.com>
2024-01-06 00:28:00 +0530

    assignment 03 completed

--- Week 3/Assignments/03-mongo/routes/admin.js
+++ Week 3/Assignments/03-mongo/routes/admin.js
@@ -1,18 +1,50 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin } = require("../db");
+const { Course } = require("../db");
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
-    // Implement admin signup logic
+router.post("/signup", async (req, res) => {
+  // Implement admin signup logic
+  const username = req.body.username;
+  const password = req.body.password;
+  const adminExist = await Admin.findOne({
+    username: username,
+  });
+  if (!adminExist) {
+    await Admin.create({
+        username:username,
+        password:password
+    });
+    res.json({
+      message: "Admin created successfully",
+    });
+  } else {
+    res.status(404).send("adim already exist");
+  }
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
-    // Implement course creation logic
+router.post("/courses", adminMiddleware,async (req, res) => {
+  // Implement course creation logic
+  const course = {
+    title: req.body.title,
+    description: req.body.description,
+    price: req.body.price,
+    imageLink: req.body.imageLink,
+  };
+  const newCourse =await Course.create(course);
+  res.json({
+    message: 'Course created successfully', courseId: newCourse._id
+  })
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
-    // Implement fetching all courses logic
+router.get("/courses", adminMiddleware, async (req, res) => {
+  // Implement fetching all courses logic
+  const courses = await Course.find({});
+  res.json({
+    courses:courses
+  })
 });
 
 module.exports = router;
--- Week 3/Assignments/03-mongo/routes/user.js
+++ Week 3/Assignments/03-mongo/routes/user.js
@@ -1,22 +1,69 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { User, Course } = require("../db");
 
+//refer the solution objectId method is used there
 // User Routes
-router.post('/signup', (req, res) => {
-    // Implement user signup logic
+router.post("/signup", async (req, res) => {
+  // Implement user signup logic
+  const username = req.body.username;
+  const password = req.body.password;
+  const userExist = await User.findOne({
+    username: username,
+  });
+  if (!userExist) {
+    await User.create({
+      username: username,
+      password: password,
+      purchasedCourses: [],
+    });
+    res.json({
+      message: "User created successfully",
+    });
+  } else {
+    res.status(404).send("user already exist");
+  }
 });
 
-router.get('/courses', (req, res) => {
-    // Implement listing all courses logic
+router.get("/courses", async (req, res) => {
+  // Implement listing all courses logic
+  const courses = await Course.find({});
+  res.json({
+    courses: courses,
+  });
 });
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
-    // Implement course purchase logic
+router.post("/courses/:courseId", userMiddleware, async (req, res) => {
+  // Implement course purchase logic
+  const courseId = req.params.courseId;
+  await User.updateOne(
+    {
+      username: req.headers.username,
+    },
+    {
+      purchasedCourses: courseId,
+    }
+  );
+  res.json({ message: "Course purchased successfully" });
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
-    // Implement fetching purchased courses logic
+router.get("/purchasedCourses", userMiddleware, async (req, res) => {
+  // Implement fetching purchased courses logic
+  const user = await User.findOne({
+    username: req.headers.username,
+  });
+  let purchasedCoursesId = user.purchasedCourses;
+  const purchasedCourses = await Promise.all(
+    purchasedCoursesId.map(async (element) => {
+      const course=await Course.findOne({
+        _id: element,
+      });
+      return course
+    })
+  );
+
+  res.json({ purchasedCourses: purchasedCourses });
 });
 
-module.exports = router
+module.exports = router;
