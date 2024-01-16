f9605fccd13627fb102e448124c7826f7e8fcfd2
Habeel-Shamsudeen <habeelshamsudeen9895@gmail.com>
2024-01-16 22:47:59 +0530

    Week 4 assignment completed

---
+++ Week 4/Assignments/react-basics/Todo-Project/src/App.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+let globalId = 0;
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  function addTodo() {
+    const title = document.getElementById("title").value;
+    const description = document.getElementById("description").value;
+    const id = globalId++;
+    const newTodo = {
+      id: id,
+      title: title,
+      description: description,
+      done: false,
+    };
+
+    setTodos((prevTodos) => [...prevTodos, newTodo]);
+  }
+
+  function markAsDone(id) {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => (todo.id === id ? { ...todo, done: true } : todo))
+    );
+  }
+  return (
+    <div className="App">
+      <h3>Todo Title</h3>
+      <input
+        type="text"
+        id="title"
+        placeholder="Todo title"
+        className="input"
+      />
+      <h3>Todo description</h3>
+      <input
+        type="text"
+        id="description"
+        placeholder="Todo description"
+        className="input"
+      />
+      <br />
+      <br />
+      <button onClick={addTodo} className="addTodo">
+        Add todo
+      </button>
+      <hr />
+      <div id="todos" class="container">
+        {todos.map((todo) => {
+          return (
+            <div key={todo.id} className="card">
+              <div className="title">{todo.title}</div>
+              <div className="desc">{todo.description}</div>
+              {!todo.done ? (
+                <button onClick={() => markAsDone(todo.id)}>
+                  Mark As Done
+                </button>
+              ) : (
+                <button>Done!</button>
+              )}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
