9ecce218d709fba3046f7fff72ec73155fd4e419
Habeel-Shamsudeen <habeelshamsudeen9895@gmail.com>
2024-01-21 21:04:39 +0530

    6.1

--- Week 6/6.1:React Hooks/react-app/src/App.jsx
+++ Week 6/6.1:React Hooks/react-app/src/App.jsx
@@ -1,17 +1,77 @@
-import { useState } from 'react'
-import './App.css'
+import { memo, useState } from "react";
+import "./App.css";
+
+let globalId=4;
 
 function App() {
-  return <div>
-    <Header title="hello world"></Header>
-    <Header title="hello how are you"></Header>
-  </div>
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "hello",
+      description: "desc 1",
+    },
+    {
+      id: 2,
+      title: "hello2",
+      description: "desc 2",
+    },
+    {
+      id: 3,
+      title: "hello3",
+      description: "desc 3",
+    },
+  ]);
+
+  function addTodo() {
+    setTodos([
+      ...todos,
+      {
+        id: globalId++,
+        title: Math.random(),
+        description: Math.random(),
+      },
+    ]);
+  }
+  return (
+    <div>
+      <HeaderWithButtons></HeaderWithButtons>
+      <Header title="hello how are you"></Header>
+      <button onClick={addTodo}>Add new Todo</button>
+      {todos.map((todo) => (
+        <Todo title={todo.title} description={todo.description} key={todo.id}/>
+      ))}
+    </div>
+  );
+}
+function HeaderWithButtons() {
+  const [title, setTitle] = useState("Habeel");
+  function updateTitle() {
+    setTitle("My name is " + Math.random());
+  }
+  return (
+    <div>
+      <button onClick={updateTitle}>Click me to Change the title</button>
+      <Header title={title}></Header>
+      <HeaderWithMemo title="this is a memo componetn" />
+      <HeaderWithMemo title="this is a memo componetn" />
+    </div>
+  );
 }
 
-function Header({title}){
-  return <div>
-    {title}
-  </div>
+function Header({ title }) {
+  return <div>{title}</div>;
 }
 
-export default App
+const HeaderWithMemo = memo(({ title }) => {
+  return <div>{title}</div>;
+});
+
+function Todo({ title, description }) {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <p>{description}</p>
+    </div>
+  );
+}
+export default App;
