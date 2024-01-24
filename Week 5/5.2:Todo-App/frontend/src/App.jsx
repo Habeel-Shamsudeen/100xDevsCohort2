26d60e062f05a9cd72db447878c119ac7d4a425a
Habeel-Shamsudeen <habeelshamsudeen9895@gmail.com>
2024-01-24 21:33:36 +0530

    6.1 completed

--- Week 5/5.2:Todo-App/frontend/src/App.jsx
+++ Week 5/5.2:Todo-App/frontend/src/App.jsx
@@ -2,14 +2,25 @@ import { useState } from "react";
 import "./App.css";
 import { CreateTodo } from "./components/CreateTodo";
 import { Todos } from "./components/Todos";
+import { useEffect } from "react";
 
 function App() {
   const [todos, setTodos] = useState([]);
-  //not the right way to send req in react
-  fetch("http://localhost:3000/todos").then(async (res) => {
-    const json = await res.json();
-    setTodos(json.todos);
-  });
+  //not the right way to send req in react use useEffect
+  useEffect(()=>{
+    fetch("http://localhost:3000/todos").then(async (res) => {
+      const json = await res.json();
+      setTodos(json.todos);
+    });
+    setInterval(()=>{
+      fetch("http://localhost:3000/todos").then(async (res) => {
+      const json = await res.json();
+      setTodos(json.todos);
+    });
+    },2000)
+
+  },[]);
+    
   return (
     <div>
       <CreateTodo />
--- Week 6/6.1:React Hooks/react-app/src/App.jsx
+++ Week 6/6.1:React Hooks/react-app/src/App.jsx
@@ -1,75 +1,136 @@
-import { memo, useState } from "react";
+import { memo, useEffect, useState } from "react";
 import "./App.css";
 
-let globalId=4;
+// re-render,key, Wrapper components
+// let globalId=4;
+
+// function App() {
+//   const [todos, setTodos] = useState([
+//     {
+//       id: 1,
+//       title: "hello",
+//       description: "desc 1",
+//     },
+//     {
+//       id: 2,
+//       title: "hello2",
+//       description: "desc 2",
+//     },
+//     {
+//       id: 3,
+//       title: "hello3",
+//       description: "desc 3",
+//     },
+//   ]);
+
+//   function addTodo() {
+//     setTodos([
+//       ...todos,
+//       {
+//         id: globalId++,
+//         title: Math.random(),
+//         description: Math.random(),
+//       },
+//     ]);
+//   }
+//   return (
+//     <div>
+//       <HeaderWithButtons></HeaderWithButtons>
+//       <Header title="hello how are you"></Header>
+//       <button onClick={addTodo}>Add new Todo</button>
+//       {todos.map((todo) => (
+//         <Todo title={todo.title} description={todo.description} key={todo.id}/>
+//       ))}
+//     </div>
+//   );
+// }
+// function HeaderWithButtons() {
+//   const [title, setTitle] = useState("Habeel");
+//   function updateTitle() {
+//     setTitle("My name is " + Math.random());
+//   }
+//   return (
+//     <div>
+//       <button onClick={updateTitle}>Click me to Change the title</button>
+//       <Header title={title}></Header>
+//       <HeaderWithMemo title="this is a memo componetn" />
+//       <HeaderWithMemo title="this is a memo componetn" />
+//     </div>
+//   );
+// }
+
+// function Header({ title }) {
+//   return <div>{title}</div>;
+// }
+
+// const HeaderWithMemo = memo(({ title }) => {
+//   return <div>{title}</div>;
+// });
+
+// function Todo({ title, description }) {
+//   return (
+//     <div>
+//       <h2>{title}</h2>
+//       <p>{description}</p>
+//     </div>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <div>
+//       <CardWrapper>
+//         <TextComponent/>
+//       </CardWrapper>
+
+//       <CardWrapper>
+//         <div>
+//           hello
+//         </div>
+//       </CardWrapper>
+//     </div>
+//   );
+// }
+
+// function CardWrapper({ children }) {
+//   return <div style={{ border: "2px solid black" }}>{children}</div>;
+// }
+
+// function TextComponent() {
+//   return <div>hi there</div>;
+// }
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      title: "hello",
-      description: "desc 1",
-    },
-    {
-      id: 2,
-      title: "hello2",
-      description: "desc 2",
-    },
-    {
-      id: 3,
-      title: "hello3",
-      description: "desc 3",
-    },
-  ]);
+  const [todos, setTodos] = useState([]);
 
-  function addTodo() {
-    setTodos([
-      ...todos,
-      {
-        id: globalId++,
-        title: Math.random(),
-        description: Math.random(),
-      },
-    ]);
-  }
+  useEffect(() => {
+    setInterval(() => {
+      fetch("https://sum-server.100xdevs.com/todos").then(async (res) => {
+        const response = await res.json();
+        setTodos(response.todos);
+      });
+    }, 5 * 1000);
+  }, []);
+
   return (
     <div>
-      <HeaderWithButtons></HeaderWithButtons>
-      <Header title="hello how are you"></Header>
-      <button onClick={addTodo}>Add new Todo</button>
-      {todos.map((todo) => (
-        <Todo title={todo.title} description={todo.description} key={todo.id}/>
-      ))}
+      {todos.map((todo) => {
+        return (
+          <Todo
+            title={todo.title}
+            description={todo.description}
+            key={todo.id}
+          />
+        );
+      })}
     </div>
   );
 }
-function HeaderWithButtons() {
-  const [title, setTitle] = useState("Habeel");
-  function updateTitle() {
-    setTitle("My name is " + Math.random());
-  }
-  return (
-    <div>
-      <button onClick={updateTitle}>Click me to Change the title</button>
-      <Header title={title}></Header>
-      <HeaderWithMemo title="this is a memo componetn" />
-      <HeaderWithMemo title="this is a memo componetn" />
-    </div>
-  );
-}
-
-function Header({ title }) {
-  return <div>{title}</div>;
-}
-
-const HeaderWithMemo = memo(({ title }) => {
-  return <div>{title}</div>;
-});
 
 function Todo({ title, description }) {
   return (
     <div>
-      <h2>{title}</h2>
+      <h3>{title}</h3>
       <p>{description}</p>
     </div>
   );
