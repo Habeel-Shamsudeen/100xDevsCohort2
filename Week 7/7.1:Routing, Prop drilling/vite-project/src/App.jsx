55d3b71997fe015c5c28af382313b3bbb8a2d7ce
Habeel-Shamsudeen <habeelshamsudeen9895@gmail.com>
2024-01-30 23:47:03 +0530

    7.1 added

---
+++ Week 7/7.1:Routing, Prop drilling/vite-project/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import {BrowserRouter,Routes,Route, useNavigate} from 'react-router-dom'
+import './App.css'
+import { Dashboard } from './components/Dashboard'
+import { Landing } from './components/Landing'
+
+function App() {
+  const navigate = useNavigate()
+  return (
+    <div>
+      <div>
+        <button onClick={()=>{
+          navigate('/');
+        }}>Landing Page</button>
+        <button onClick={()=>{
+          navigate('/dashboard');
+        }}>Dashboard</button>
+      </div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/' element={<Landing/>}/>
+      </Routes>
+    </BrowserRouter>
+    </div>
+    
+  )
+}
+
+export default App
