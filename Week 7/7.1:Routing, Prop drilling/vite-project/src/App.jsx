92f2409fdf055a927a43cd9b12ee24846501ac6f
Habeel-Shamsudeen <habeelshamsudeen9895@gmail.com>
2024-01-31 22:58:04 +0530

    7.1 added

--- Week 7/7.1:Routing, Prop drilling/vite-project/src/App.jsx
+++ Week 7/7.1:Routing, Prop drilling/vite-project/src/App.jsx
@@ -12,7 +12,7 @@ import { CountContext } from "./context";
 //         <Appbar />
 //         <Routes>
 //           <Route path="/dashboard" element={<Suspense fallback={'loading..'}><Dashboard /></Suspense>} />
-//           <Route path="/" element={<Suspense fallback={'loading..'}><Landing /></Suspense>} /> 
+//           <Route path="/" element={<Suspense fallback={'loading..'}><Landing /></Suspense>} />
 //           {/* suspense api, async comp fetching, async data fetching */}
 //         </Routes>
 //       </BrowserRouter>
@@ -42,43 +42,52 @@ import { CountContext } from "./context";
 //   );
 // }
 
-
-function App(){
-  const [count,setCount] = useState(0);
+function App() {
+  const [count, setCount] = useState(0);
   // wrap anyone that wants to use the teleported value inside a provider
-  return <div>
-    <CountContext.Provider value={count}>
-    <Count  count={count} setCount={setCount}/>
-    </CountContext.Provider>
-    
-    
-  </div>
+  return (
+    <div>
+      <CountContext.Provider value={count}>
+        <Count count={count} setCount={setCount} />
+      </CountContext.Provider>
+    </div>
+  );
 }
 
-function Count({setCount}){
-  return <div>
-    <CountRender/>
-    <Buttons setCount={setCount}/>
-  </div>
+function Count({ setCount }) {
+  return (
+    <div>
+      <CountRender />
+      <Buttons setCount={setCount} />
+    </div>
+  );
 }
 
-function CountRender(){
-  const count = useContext(CountContext)
-  return <div>
-    {count}
-  </div>
+function CountRender() {
+  const count = useContext(CountContext);
+  return <div>{count}</div>;
 }
 
-function Buttons({setCount}){
-  const count = useContext(CountContext)
-  return <div>
-    <button onClick={()=>{
-      setCount(count+1)
-    }}>Increase</button>
-    <button onClick={()=>{
-      setCount(count-1)
-    }}>Decrease</button>
-  </div>
+function Buttons({ setCount }) {
+  const count = useContext(CountContext);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increase
+      </button>
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        Decrease
+      </button>
+    </div>
+  );
 }
 
 export default App;
