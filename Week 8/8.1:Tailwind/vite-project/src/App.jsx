17ef5e6b348b4614e53c57aacd32bc2e6b19d691
Habeel-Shamsudeen <habeelshamsudeen9895@gmail.com>
2024-02-11 09:43:32 +0530

    8.1 completed

--- Week 8/8.1:Tailwind/vite-project/src/App.jsx
+++ Week 8/8.1:Tailwind/vite-project/src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { RevenueCard } from './components/RevenueCard'
+import { SideBar } from './components/SideBar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className='grid grid-cols-12'>
-      <div className='bg-red-500 col-span-12 sm-col-span-7'>hello</div>
-      <div className='bg-green-500 col-span-12 sm-col-span-3'>how</div>
-      <div className='bg-yellow-500 col-span-12 sm-col-span-2'>are you</div>
-
+    <div className='grid grid-cols-1 sm:grid-cols-2'>
+      <SideBar/>
+      <RevenueCard title={"Amount pending"} amount={"92,312.20"} orderCount={13} />
     </div>
   )
 }
