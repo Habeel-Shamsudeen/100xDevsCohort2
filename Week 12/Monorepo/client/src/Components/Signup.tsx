6a0f38869cb0c7845314bc318b9f47b5393bf732
Habeel-Shamsudeen <habeelshamsudeen9895@gmail.com>
2024-03-31 18:12:12 +0530

    mjinor changes

--- Week 12/Monorepo/client/src/Components/Signup.tsx
+++ Week 12/Monorepo/client/src/Components/Signup.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import {Link, useNavigate} from 'react-router-dom';
 import {useSetRecoilState} from "recoil";
 import {authState} from "../store/authState.js";
