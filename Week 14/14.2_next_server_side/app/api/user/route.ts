d5a2c9f59fd4f55e14ed86661b8daa2a11b0b597
Habeel-Shamsudeen <habeelshamsudeen9895@gmail.com>
2024-04-17 19:08:33 +0530

    14.2 added

--- Week 14/14.2_next_server_side/app/api/user/route.ts
+++ Week 14/14.2_next_server_side/app/api/user/route.ts
@@ -1,3 +1,18 @@
-export async function GET() {
-    return Response.json({ name: "harkirat", email: "harkirat@gmail.com" })
-  }
+import { NextRequest, NextResponse } from 'next/server';
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export async function POST(req: NextRequest) {
+  
+    const body = await req.json();
+    const user = await prisma.user.create({
+      data: {
+          username: body.username,
+          password: body.password
+      }
+  });
+
+  console.log(user.id);
+  return NextResponse.json({ message: "Signed up" });
+}
